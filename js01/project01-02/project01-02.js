ab39f89cfb445fb8aae89bf58957a942bfb3ea93
nadiag01 <nadiagainer@gmail.com>
2024-01-14 10:49:26 -0600

    placed variables and added to table

--- js01/project01-02/project01-02.js
+++ js01/project01-02/project01-02.js
@@ -3,9 +3,17 @@
     Hands-On Project 1-2
 
     Author: Nadia Gainer
-    Date: 01/13/24  
+    Date: 01/13/24
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+let service1Name="Basic";
+let service2Name = "Express";
+let service3Name="Extreme";
+let service4Name = "Ultimate";
+let service1Speed ="0";
+let service2Speed ="100";
+let service3Speed = "500";
+let service4Speed ="1";
