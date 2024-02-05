c98cb864cc023e2e93c8afeab63f7669e94d6caa
nadiag01 <nadiagainer@gmail.com>
2024-02-04 18:56:43 -0600

    updating files

---
+++ js03/project03-01/project03-01.js
@@ -0,0 +1,32 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Nadia Gainer
+      Date:   2/2/24
+
+      Filename: project03-01.js
+*/
+
+let menuItems = document.getElementsByClassName("menuItem");
+for(let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click",
+  calcTotal()
+  let i=counter);
+ }
+
+
+function calcTotal("menuItem"){
+  let orderTotal = 0;
+  for(i = 0; i < 5; i++) {
+  if(menuItems[i].checked) 
+  orderTotal += (Number(menuItems[i].value);
+  }
+  document.getElementById("billTotal").innerHTML = formatCurrency(orderTotal);
+
+
+ // Function to display a numeric value as a text string in the format $##.## 
+ function formatCurrency(value) {
+  return "$" + value.toFixed(2);
+ }
