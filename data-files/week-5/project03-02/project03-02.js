440329a7a838416a67d0d45f491b0995a41bbb98
nadiag01 <nadiagainer@gmail.com>
2024-02-11 16:43:48 -0600

    feat: updated coding

--- data-files/week-5/project03-02/project03-02.js
+++ data-files/week-5/project03-02/project03-02.js
@@ -3,8 +3,8 @@
       Project 03-02
 
       Application to generate a slide gallery
-      Author: 
-      Date:   
+      Author: Nadia Gainer
+      Date:  02/10/24 
 
       Filename: project03-02.js
 */
@@ -25,4 +25,15 @@ captions[11]="The International Space Station second expansion [2006]";
 captions[12]="The International Space Station third expansion [2007]";
 captions[13]="The ISS over the Ionian Sea [2007]";
 
+let htmlCode = "";
+document.write("<figure></figure>")
 
+for (let i = 0; i < captions.length; i++) {
+  <figure>
+    <img alt='' src='slidei.jpg' />   
+    <figcaption>captions[i]</figcaption> 
+  </figure>
+}
+
+
+document.getElementById("gallery").innerHTML = htmlCode;
